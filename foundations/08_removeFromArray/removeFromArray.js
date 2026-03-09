e9ca99a5ec338e2f7fde5e46493eb92e55495a86
Mr-Shoez <molelekishoez@gmail.com>
2026-03-09 04:44:13 +0200

    complete : create function that basically takes a mulitple paramenters , including an array and removes the parameters from the array

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...itemsToRemove) {
+  if (!Array.isArray(arr)) {
+    throw new TypeError("First Parameters must be arrays");
+  }
+
+  let filteredArray = arr.filter((item) => !itemsToRemove.includes(item));
+  return filteredArray;
 };
 
 // Do not edit below this line
